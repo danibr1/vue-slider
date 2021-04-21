121264c644eb67fd09b62a01fed960a277707a24
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-04-21 23:50:21 +0200

    html completed

---
+++ js/main.js
@@ -0,0 +1,48 @@
+/*********************************************************
+ *  VUE SLIDER
+ *********************************************************/
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        // IMAGES ARRAY
+        photos: [
+            './img/1.jpg',
+            './img/2.jpg',
+            './img/3.jpg',
+            './img/4.jpg'
+        ],
+        // INITIAL INDEX
+        indexPhoto: 0,
+        intervalID: 0,
+    },
+    created (){
+        // ISTANZA VUE
+        this.startLoop();
+    },
+    methods: {
+        // CLICK PREV PHOTO
+        prevPhoto () {
+            this.indexPhoto -= 1;
+
+            // CHECK END
+            if(this.indexPhoto < 0) {
+                this.indexPhoto = this.photos.length -1
+            }
+        },
+        // CLICK NEXT PHOTO
+        nextPhoto () {
+            this.indexPhoto += 1;
+
+            // CHECK END
+            if(this.indexPhoto > (this.photos.length -1) ) {
+                this.indexPhoto = 0;
+            }
+        },
+        // SET PHOTO
+        setPhoto (index) {
+            this.indexPhoto = index;
+        }
+    }
+});
+
